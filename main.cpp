4ce0199fc8b5b4fd01429571e52d3968fe9e9b5d
CODSensei <krishna2342005@gmail.com>
2024-03-13 22:16:10 +0530

    basic-q6

--- main.cpp
+++ main.cpp
@@ -2,9 +2,14 @@
 using namespace std;
 
 int main(){
-    cout << sizeof(int)<<endl;
-    cout << sizeof(float)<<endl;
-    cout << sizeof(double)<<endl;
-    cout << sizeof(char)<<endl;
+    int x,y;
+    cin >> x >>y;
+    cout <<" Numbers before swap " << x << "\t"<<y<<endl;
+    int t = x;
+    x=y;
+    y=t;
+    cout <<" Numbers after swap " << x << "\t"<<y;
+     
+
 
 }
