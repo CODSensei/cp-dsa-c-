1e2fcd8e96dd8f375c478361f869d6bc995302ab
CODSensei <krishna2342005@gmail.com>
2024-03-13 22:32:38 +0530

    if-q3

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main(){
-    char c;
-    cin >> c;
-    if (c == 'a'||c=='e'||c=='i'||c=='o'||c=='u'||c == 'A'||c=='E'||c=='I'||c=='O'||c=='U')
-    cout << "Vowel" ;
-    else 
-    cout << "Consonent";
+    int a,b,c;
+    cin >> a>>b>>c;
+    if (a>b){
+        if (a>c)
+        cout << "Largest number is "<< a;
+        else
+        cout << "Largest number is "<< c;
+    }
+    else {
+        if (b>c)
+        cout << "Largest number is "<< b;
+        else
+        cout << "Largest number is "<< c;
+
+    }
 }
