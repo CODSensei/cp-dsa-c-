7f636b258828767e2950e50a47552a9c17db57df
CODSensei <krishna2342005@gmail.com>
2024-03-13 22:17:35 +0530

    basic-q7

--- main.cpp
+++ main.cpp
@@ -2,13 +2,9 @@
 using namespace std;
 
 int main(){
-    int x,y;
-    cin >> x >>y;
-    cout <<" Numbers before swap " << x << "\t"<<y<<endl;
-    int t = x;
-    x=y;
-    y=t;
-    cout <<" Numbers after swap " << x << "\t"<<y;
+    char c;
+    cin >> c;
+    cout << (int)c;
      
 
 
