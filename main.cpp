87b1a1fad97e51a2d36616c54ba611b58eb04ad6
CODSensei <krishna2342005@gmail.com>
2024-03-13 22:02:22 +0530

    basic-q1

--- main.cpp
+++ main.cpp
@@ -1,14 +1,6 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
-int main() {
-    int n = 0;
-    for (int i = 0; i<=10 ; i++)
-    {
-        for (int j =0 ; j<=i ; j++)
-        cout << '*';
-        cout <<endl;
-    }
-    
-    
+int main(){
+    cout << "Hello World";
 }
