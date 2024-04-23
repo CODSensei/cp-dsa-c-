95a65d2264c071ed92fac362eb83e66321c500ef
CODSensei <krishna2342005@gmail.com>
2024-04-24 00:20:06 +0530

    striver-patterns-N/2-forest

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
-void nForest(int n) {
-	// Write your code here.
-	if (n>=1 || n<=25){
-		for (int i = 0 ; i <n ;i++){
-			for (int j = 0 ; j< n ; j++ )
-			cout<< "* ";
-			cout << '\n';
-		}
-	}
+void nForest(int n)
+{
+    // Write your code here.
+    if (n >= 1 || n <= 25)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j <= i; j++)
+                cout << "* ";
+            cout << "\n";
+        }
+    }
 }
