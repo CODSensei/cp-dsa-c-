e9a08a882cabe1611a2757a92709c0d3cea0a812
CODSensei <krishna2342005@gmail.com>
2024-04-24 00:23:17 +0530

    striver-patterns-N-Triangle

--- main.cpp
+++ main.cpp
@@ -1,13 +1,10 @@
-void nForest(int n)
-{
-    // Write your code here.
-    if (n >= 1 || n <= 25)
-    {
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j <= i; j++)
-                cout << "* ";
-            cout << "\n";
-        }
-    }
+void nTriangle(int n) {
+	// Write your code here
+	if (n>=1 || n<=25){
+		for (int i = 0 ; i<n; i++){
+			for (int j=0 ; j<=i ; j++)
+			cout << j+1 << ' ';
+			cout << "\n";
+		}
+	}
 }
