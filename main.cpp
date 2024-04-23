3989cba75877f00f88810f7f57a155a8c5826fc1
CODSensei <krishna2342005@gmail.com>
2024-04-24 00:15:43 +0530

    striver-patterns-N-forest

--- main.cpp
+++ main.cpp
@@ -1,47 +1,10 @@
-#include<iostream>
-
-#include<cmath>
-
-#include<vector>
-
- 
-
-double areaSwitchCase(int ch, std::vector<double> a) {
-
-  double area = 0.0;
-
- 
-
-  switch(ch){
-
- 
-
-        case 1: {
-
-          double pi = M_PI;
-
-          area = pi * (a[0] * a[0]);
-
-          break;
-
-        }
-
- 
-
-        case 2: {
-
-          area = a[0] * a[1];
-
-          break;
-
-        }
-
-        default:
-
-          std::cout<< "Incvalid choice\n";
-
-        }
-
-        return area;
-
+void nForest(int n) {
+	// Write your code here.
+	if (n>=1 || n<=25){
+		for (int i = 0 ; i <n ;i++){
+			for (int j = 0 ; j< n ; j++ )
+			cout<< "* ";
+			cout << '\n';
+		}
+	}
 }
