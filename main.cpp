7b04d11ce8914c1b5b74c35a7f597aed7e78246b
CODSensei <krishna2342005@gmail.com>
2024-06-07 23:38:19 +0530

    striver-patterns-reverse-number-triangle

--- main.cpp
+++ main.cpp
@@ -1,23 +1,30 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 void nTriangle(int n);
 
-
-
-
-void nTriangle(int n) {
+void nTriangle(int n)
+{
 	// Write your code here
-	if (n>=1 || n<=25){
-		for (int i = n ; i>0; i--){
-			for (int j=i ; j>0 ; j--)
-			cout << "*" << " ";
+	if (n >= 1 || n <= 25)
+	{
+		int c = 1;
+		for (int i = 0; i <= n; i++)
+		{
+			for (int j = n; j > i; j--)
+			{
+				cout << c << " ";
+				c++;
+			}
 			cout << "\n";
+			c = 1;
 		}
 	}
-}int main() {
-    int n;
-    cin >> n;
-    nTriangle(n);
-    return 0;
+}
+int main()
+{
+	int n;
+	cin >> n;
+	nTriangle(n);
+	return 0;
 }
