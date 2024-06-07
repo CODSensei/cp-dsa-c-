432738cce79dc56ec6c51e956eb4b486d4a3e75c
CODSensei <krishna2342005@gmail.com>
2024-06-07 23:28:52 +0530

    striver-patterns-triangle

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void nTriangle(int n);
+
+
+
+
 void nTriangle(int n) {
 	// Write your code here
 	if (n>=1 || n<=25){
 		for (int i = 0 ; i<n; i++){
 			for (int j=0 ; j<=i ; j++)
-			cout << j+1 << ' ';
+			cout << i+1 << " ";
 			cout << "\n";
 		}
 	}
+}int main() {
+    int n;
+    cin >> n;
+    nTriangle(n);
+    return 0;
 }
