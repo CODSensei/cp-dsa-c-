2191e005c20f581f3409881aaddf8619c4f7080c
CODSensei <krishna2342005@gmail.com>
2024-06-08 15:55:38 +0530

    striver-patterns-star-triangle

--- main.cpp
+++ main.cpp
@@ -6,21 +6,27 @@ void nTriangle(int n);
 void nTriangle(int n)
 {
 	// Write your code here
-	if (n >= 1 || n <= 25)
+	if (n >= 1 || n <= 20)
 	{
-		int c = 1;
-		for (int i = 0; i <= n; i++)
+		for (int i = 0; i < n; i++)
 		{
-			for (int j = n; j > i; j--)
+			for (int j = 0; j < n - i - 1; j++)
 			{
-				cout << c << " ";
-				c++;
+				cout << " ";
 			}
-			cout << "\n";
-			c = 1;
+			for (int j = 0; j < 2 * i + 1; j++)
+			{
+				cout << "*";
+			}
+			for (int j = 0; j < n - i - 1; j++)
+			{
+				cout << " ";
+			}
+			cout << endl;
 		}
 	}
 }
+
 int main()
 {
 	int n;
