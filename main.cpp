ae62a79fb5360db32d2b21ea676510e8120e029a
CODSensei <krishna2342005@gmail.com>
2024-06-15 01:35:18 +0530

    striver-pattern-reverse-star-triangle

--- main.cpp
+++ main.cpp
@@ -8,17 +8,17 @@ void nTriangle(int n)
 	// Write your code here
 	if (n >= 1 || n <= 20)
 	{
-		for (int i = 0; i < n; i++)
+		for (int i = n; i > 0; i--)
 		{
-			for (int j = 0; j < n - i - 1; j++)
+			for (int j = n - i - 1; j >= 0; j--)
 			{
 				cout << " ";
 			}
-			for (int j = 0; j < 2 * i + 1; j++)
+			for (int j = 2 * i - 1; j > 0; j--)
 			{
 				cout << "*";
 			}
-			for (int j = 0; j < n - i - 1; j++)
+			for (int j = n - i - 1; j >= 0; j--)
 			{
 				cout << " ";
 			}
